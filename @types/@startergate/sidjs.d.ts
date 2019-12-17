3ff5dd74ad703085007ac3543142b71d0fef74f5
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-18 03:00:25 +0900

    [UPDATE] sidjs.d.ts complete

--- @types/@startergate/sidjs.d.ts
+++ @types/@startergate/sidjs.d.ts
@@ -1,9 +1,17 @@
 declare module '@startergate/sidjs' {
     global {
         interface SID {
-            constructor(clientName:string): never;
+            constructor(clientName:string): null;
             login(clientid:string, id:string, pw:string): Promise<object>;
-            loginAuth(clientid:string, sessid:string): Promise<object>;
+            loginAuth(clientid:string, sessid:string): Promise<object> | null;
+            logout(clientid:string, sessid:string): Promise<object> | null;
+            register(clientid:string, userid:string, pw:string, nickname:string | null): Promise<string|Error>;
+            getUserNickname(clientid:string, sessid:string): Promise<string|Error>;
+            loginCheck(target:any): never;
+            passwordCheck(clientid:string, sessid:string, pw:string): Promise<boolean|Error>;
+            createClientID(devicedata:string): Promise<string>;
+            setClientName(clientName:string): null;
+            getClientName(): string;
         }
     }
 
