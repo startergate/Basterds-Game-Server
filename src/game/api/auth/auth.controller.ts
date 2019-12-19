b24ebb232520177268aeed3b4f29d8c2ae9392e1
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-19 10:55:33 +0900

    [UPDATE] sidjs -> sidts

--- src/game/api/auth/auth.controller.ts
+++ src/game/api/auth/auth.controller.ts
@@ -1,4 +1,7 @@
+import SID from "@startergate/sidts"
+
 export const sessionReceiver = (ctx : any) => {
+
     ctx.body = {
         sessid: "some sessid",
         pid: "somepid"
--- src/game/api/auth/index.ts
+++ src/game/api/auth/index.ts
@@ -1,9 +1,12 @@
 import * as Router from 'koa-router';
-import * as sid from "@startergate/sidjs";
+import SID from '@startergate/sidts'
+
 import {sessionReceiver} from "./auth.controller";
 
 const auth = new Router();
 
+
+const sid = new SID('basterds_game');
 auth.post('session', sessionReceiver);
 
 export default auth;
