463db0d9d932f0db4a1901a3de275f15d43934f9
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-23 07:16:16 +0900

    [CREATE] Basic data verification

--- src/game/api/auth/auth.controller.ts
+++ src/game/api/auth/auth.controller.ts
@@ -5,6 +5,19 @@ import * as Joi from "joi"
 const sid = new SID("basterds-game");
 
 export const sessionReceiver = (ctx : any) => {
+    const request = Joi.object().keys({
+        id: Joi.string().required(),
+        pw: Joi.string().required()
+    });
+
+    const result = Joi.validate(ctx.request.body, request);
+
+    if (!result) {
+        ctx.body = {is_succeed: false};
+        ctx.status = 400;
+        return;
+    }
+
 
     ctx.body = {
         is_succeed: true,
