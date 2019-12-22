f8c23a7ed6f260757056777d8ca5ea10466e3ccc
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-23 07:24:53 +0900

    [UPDATE] create login

--- src/game/api/auth/auth.controller.ts
+++ src/game/api/auth/auth.controller.ts
@@ -1,13 +1,13 @@
 import SID from "@startergate/sidts"
 import * as Joi from "joi"
 
-
 const sid = new SID("basterds-game");
 
-export const sessionReceiver = (ctx : any) => {
+export const sessionReceiver = async (ctx : any) => {
     const request = Joi.object().keys({
         id: Joi.string().required(),
-        pw: Joi.string().required()
+        pw: Joi.string().required(),
+        clientid: Joi.string()
     });
 
     const result = Joi.validate(ctx.request.body, request);
@@ -18,12 +18,16 @@ export const sessionReceiver = (ctx : any) => {
         return;
     }
 
+    const clientid = ctx.request.body.clientid ? ctx.request.body.clientid : await sid.createClientID("basterds-game");
+
+    const session = await sid.login(clientid, ctx.request.body.id, ctx.request.body.pw);
 
     ctx.body = {
         is_succeed: true,
         data: {
-            sessid: "some sessid",
-            pid: "somepid"
+            sessid: session.sessid,
+            pid: session.pid,
+            clientid: clientid
         }
     }
 };
