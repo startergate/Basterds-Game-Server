bac3cffbed030ae9194574eb8249eab170dca818
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-26 14:56:37 +0900

    [CREATE] User Model

---
+++ src/models/User.ts
@@ -0,0 +1,27 @@
+import { Sequelize, DataTypes, Model, BuildOptions } from "sequelize";
+
+interface UserModel extends Model {
+    readonly pid: string;
+    readonly id: string;
+}
+
+type UserModelStatic = typeof Model & {
+    new (values?: object, options?: BuildOptions): UserModel;
+}
+
+export const User = (sequelize: Sequelize) => {
+    return <UserModelStatic>sequelize.define("User", {
+        pid: {
+            type: DataTypes.CHAR(64),
+            primaryKey: true
+        },
+        id: {
+            type: DataTypes.CHAR(64),
+            allowNull: true,
+            unique: true
+        }
+    }, {
+        timestamps: false,
+        tableName: "auth_sid_user"
+    });
+};
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,13 @@
 import { Sequelize } from "sequelize"
 const config = require('modules/dbInfo');
 
+import { User } from "./User"
+
 const sequelize = new Sequelize('basterds', config.id, config.pw, {
     host: config.host,
     dialect: "mysql"
 });
 
-export default {sequelize, Sequelize};
+const user = User(sequelize);
+
+export default {sequelize, Sequelize, user};
