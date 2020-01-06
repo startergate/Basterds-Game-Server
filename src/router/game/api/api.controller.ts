ea2d3a4967dfaaf64623981919fb93d2799275a9
STARTERGATE <32726809+startergate@users.noreply.github.com>
2020-01-06 17:37:13 +0900

    [CREATE] Json Test

--- src/router/game/api/api.controller.ts
+++ src/router/game/api/api.controller.ts
@@ -3,3 +3,9 @@ import { Context } from 'koa';
 export const apiIndex = (ctx: Context) => {
     ctx.body = 'BUILD 20200106';
 };
+
+export const apiJsonTest = (ctx: Context) => {
+    ctx.body = {
+        "test": 1
+    }
+};
--- src/router/game/api/index.ts
+++ src/router/game/api/index.ts
@@ -1,12 +1,13 @@
 import * as Router from 'koa-router';
 import auth from './auth';
 import v1 from './v1';
-import { apiIndex } from './api.controller';
+import { apiIndex, apiJsonTest } from './api.controller';
 
 const api = new Router();
 
 api.use('/auth', auth.routes());
 api.use('/v1', v1.routes());
 api.all('/', apiIndex);
+api.all('/json', apiJsonTest);
 
 export default api;
