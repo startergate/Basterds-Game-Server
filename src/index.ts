cf09ddd1fbc04cee778bb9aefb03e8fc9d54bead
STARTERGATE <32726809+startergate@users.noreply.github.com>
2020-01-07 09:18:18 +0900

    [CREATE] CORS stuff

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import * as bodyParser from 'koa-bodyparser';
 import * as logger from 'koa-logger';
 
 import appRouter from './router';
+import {Context} from "koa";
 
 const app = new Koa();
 app.use(logger());
@@ -13,6 +14,15 @@ const router = new Router();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser());
+
+/*app.use(async (ctx: Context, next: () => Promise<any>) => {
+  ctx.header('Access-Control-Allow-Origin', '*');
+  ctx.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  ctx.header('Access-Control-Max-Age', '3600');
+  ctx.header('Access-Control-Allow-Headers', 'Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization');
+  await next();
+});*/
+
 router.use('', appRouter.routes());
 app.use(router.routes()).use(router.allowedMethods());
 
